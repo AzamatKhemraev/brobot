65e79f80092a9d2eaf37584f7715920e67925734
AzamatKhemraev <aza200@mail.ru>
2025-04-24 00:50:59 +0300

    refactor: переход на модель deepseek/deepseek-r1:free

--- services/gpt.py
+++ services/gpt.py
@@ -13,7 +13,7 @@ client = OpenAI(
 async def chat_with_gpt(prompt: str) -> str:
     try:
         response = client.chat.completions.create(
-            model="deepseek/deepseek-chat-v3-0324:free",  # или другую модель
+            model="deepseek/deepseek-r1:free",  # или другую модель
             messages=[{"role": "user", "content": prompt}],
             temperature=0.7
         )
