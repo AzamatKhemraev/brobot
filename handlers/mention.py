71ddcf095f4473d3bda9840d4105e8c283e550e0
AzamatKhemraev <aza200@mail.ru>
2025-04-24 17:32:42 +0300

    chore:функция ответа от нейросети теперь работает в фоне

--- handlers/mention.py
+++ handlers/mention.py
@@ -1,6 +1,6 @@
 from aiogram import Router, types
 from services.gpt import chat_with_gpt
-# import asyncio
+import asyncio
 
 router = Router()
 
@@ -13,12 +13,18 @@ async def mention_gpt_reply(message: types.Message):
 
     # Проверяем, есть ли упоминание
     if any(alias in text for alias in BOT_ALIASES):
+        # запускаем асинхронную задачу в фоне
+        asyncio.create_task(respond_with_gpt(message))
+
+
+# эта функция отвечает GPT "в фоне"
+async def respond_with_gpt(message: types.Message):
+    try:
         await message.bot.send_chat_action(message.chat.id, "typing")
-
-        # GPT-ответ через OpenRouter
-        gpt_reply = await chat_with_gpt(text)
-
+        gpt_reply = await chat_with_gpt(message.text)
         await message.reply(gpt_reply)
+    except Exception as e:
+        await message.reply(f"❗ Ошибка при ответе от GPT: {e}")
 
 
 def register_handlers(dp):
