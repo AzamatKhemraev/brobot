6b3f786db17e028fa0265f90e770c99101f8ef1b
AzamatKhemraev <aza200@mail.ru>
2025-04-24 20:54:44 +0300

    fix:исправление ошибок

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 from aiogram import Bot, Dispatcher
-# from aiogram.types import ParseMode
 from config import BOT_TOKEN
-from handlers import echo, mention, gif, sticker_id_catcher
+from handlers import echo, mention
 
 import asyncio
 
@@ -14,8 +13,6 @@ async def main():
     # Подключаем маршруты
     echo.register_handlers(dp)
     mention.register_handlers(dp)
-    gif.register_handlers(dp)
-    sticker_id_catcher.register_handlers(dp)
 
     # Запускаем бота
     await dp.start_polling(bot)
