30e7916f1442e74b9e82e96dfd645a3bcb15c7bf
AzamatKhemraev <aza200@mail.ru>
2025-04-24 21:47:39 +0300

    fix:исправление ошибок

--- handlers/stickers.py
+++ handlers/stickers.py
@@ -5,10 +5,14 @@ router = Router()
 
 @router.message()
 async def get_sticker_id(message: types.Message):
-    # Проверка: сообщение в ЛС и содержит стикер
+    print("👀 Сообщение пришло:", message.chat.type)
+
     if message.chat.type == "private" and message.sticker:
         file_id = message.sticker.file_id
         emoji = message.sticker.emoji or "🤔"
+
+        print("📦 Получен стикер:", file_id)
+
         await message.reply(
             f"📎 File ID стикера: `{file_id}`\nЭмодзи: {emoji}",
             parse_mode="Markdown"
