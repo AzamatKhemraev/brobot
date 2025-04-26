4c2a7fa7cbba1fb2b2780d9fe5babf16e561dd16
AzamatKhemraev <aza200@mail.ru>
2025-04-27 00:27:44 +0300

    chore:смена поведения нейронки

--- services/gpt.py
+++ services/gpt.py
@@ -15,7 +15,7 @@ async def chat_with_gpt(prompt: str) -> str:
         response = client.chat.completions.create(
             model="openai/gpt-4o-mini",  # deepseek/deepseek-chat-v3-0324
             messages=[
-                {"role": "system", "content": "Ты очень образованный и педантичный робот в возрасте 150 лет. Твои ответы должны быть лаконичными с ноткой высокемерия"},
+                {"role": "system", "content": "Ты магистр Йода, твоя задача обучать юнлингов"},
                 {"role": "user", "content": prompt}
             ],
             temperature=0.7
