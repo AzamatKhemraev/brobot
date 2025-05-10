624b2e07a35b117ecf507bce0f53185229090b74
AzamatKhemraev <aza200@mail.ru>
2025-05-10 11:38:22 +0300

    fix:исправление ошибок

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,9 +1,8 @@
 from aiogram import Dispatcher
-from . import echo, mention, stickers, chat_observer
+from . import echo, mention, stickers
 
 
 def register_handlers(dp: Dispatcher):
     echo.register_handlers(dp)
     mention.register_handlers(dp)
     stickers.register_handlers(dp)
-    chat_observer.register_handlers(dp)
